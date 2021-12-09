45a7bdcb1d233cfe3cba672720ff983a4567d202
rptseng <tseng_r@hotmail.com>
2021-12-08 22:22:58 -0500

    created query and extracted table for mentorship eligibility

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -33,3 +33,22 @@ GROUP BY title
 ORDER BY COUNT (emp_no) DESC;
 
 SELECT * FROM retiring_titles;
+
+-- Write a query to craete a Mentorship Eligibility table
+-- Eligibility determined by birth_date between '1965-01-01' and '1965-12-31'
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp as de
+    ON e.emp_no = de.emp_no
+LEFT JOIN titles as t
+    ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY (e.emp_no);
